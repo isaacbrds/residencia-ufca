892f081e093d49198de9241cba7e2b6531f54488
isaac <isaac.brigido@gmail.com>
2023-12-14 16:27:44 -0300

    feat: another app to start with kivy

--- menu.py
+++ menu.py
@@ -5,22 +5,18 @@ from kivy.uix.label import Label
 
 class MeuApp(App):
     def build(self):
-        layout = Button(text='Clique-me!')
-        layout.bind(on_press=self.on_button_click)
+        layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
+        self.botao = Button(text='Clique-me!')
+        self.botao.bind(on_press=self.mostrar_mensagem)
+        layout.add_widget(self.botao)
+        self.label = Label(text="Pressione o Botão")
+        layout.add_widget(self.label)
+
+        
         return layout
     
-    def on_button_click(self, instance):
-        idade = 25
-
-        if idade < 18:
-            mensagem = 'Menor de Idade'
-        elif idade >= 18 and idade < 65:
-            mensagem = 'Adulto'
-        else:
-            mensagem =  'Idoso'
-
-        instance.text = mensagem
-
+    def mostrar_mensagem(self, instance):
+        self.label.text= "Você clicou no botão!"
 
 if __name__ == "__main__":
     MeuApp().run()
