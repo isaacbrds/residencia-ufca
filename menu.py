60d370b4c422c07c8817c688787791856d6a9a84
isaac <isaac.brigido@gmail.com>
2023-12-14 17:07:28 -0300

    feat: another kivy app

--- menu.py
+++ menu.py
@@ -1,22 +1,34 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 
 class MeuApp(App):
     def build(self):
+        self.tarefas = []
         layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
-        self.botao = Button(text='Clique-me!')
-        self.botao.bind(on_press=self.mostrar_mensagem)
-        layout.add_widget(self.botao)
-        self.label = Label(text="Pressione o Botão")
-        layout.add_widget(self.label)
+
+        self.input_tarefa = TextInput(hint_text='Digite uma tarefa')
+        layout.add_widget(self.input_tarefa)
+
+        adicionar_botao = Button(text='Adicionar Tarefa')
+        adicionar_botao.bind(on_press=self.adicionar_tarefa)
+        layout.add_widget(adicionar_botao)
+
+        self.lista_layout = BoxLayout(orientation='vertical', spacing=5)
+        layout.add_widget(self.lista_layout)
 
         
         return layout
     
-    def mostrar_mensagem(self, instance):
-        self.label.text= "Você clicou no botão!"
+    def adicionar_tarefa(self, instance):
+        texto_tarefa = self.input_tarefa.text
+        if texto_tarefa:
+            tarefa_label = Label(text=texto_tarefa)
+            self.lista_layout.add_widget(tarefa_label)
+            self.tarefas.append(texto_tarefa)
+            self.input_tarefa.text = ""
 
 if __name__ == "__main__":
     MeuApp().run()
